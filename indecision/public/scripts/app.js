15ca5ebafbb9ce2b31056323479ae77e1fd8bfbf
Olga Boiaryntseva <olgaboiar@gmail.com>
2018-07-25 18:34:00 -0400

    added babel with presets

--- indecision/public/scripts/app.js
+++ indecision/public/scripts/app.js
@@ -1,11 +1,13 @@
-console.log('App.js is running!')
+'use strict';
+
+console.log('App.js is running!');
 
 //JSX
 var template = React.createElement(
-    'h1',
-    null,
-    'This is JSX from App,js'
-  );
+  'h1',
+  null,
+  'MY app!'
+);
 var appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
