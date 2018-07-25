b511b39e43143ae95baafce84d8a6a7a908a284a
Olga Boiaryntseva <olgaboiar@gmail.com>
2018-07-25 19:09:38 -0400

    completed challenge

--- indecision/src/app.js
+++ indecision/src/app.js
@@ -1,7 +1,23 @@
 console.log('App.js is running!')
 
 //JSX
-var template = <h1>MY app!</h1>;
+var template = (
+    <div>
+        <h1>MY app!</h1>
+        <p>This is some info</p>
+        <ol>
+            <li>Item One</li>
+            <li>Item Two</li>
+        </ol>
+    </div>
+);
+var templateTwo = (
+    <div>
+        <h1>Olga B</h1>
+        <p>Age: 33</p>
+        <p>Location: NYC</p>
+    </div>
+);
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);
